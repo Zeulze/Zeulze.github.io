cfa86bbed2b80e2aee59e2feb198eafbe5deb711
Vladislav <gubinvladislav625@gmail.com>
2023-05-29 17:44:58 +0700

    now the list of characters gets updated by the info from the database

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,52 +1,64 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import "./charList.scss";
+import Spinner from "../spinner/spinner.jsx";
+import MarvelService from "../../services/MarvelService.jsx";
+import { useEffect, useState } from "react";
 
 const CharList = () => {
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
-}
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const service = new MarvelService();
+  let id = 0;
+
+  function onCharListLoaded(charList) {
+    const newDataList = charList.map((char) => {
+      return (
+        <li className="char__item" key={id++}>
+          <img src={char.thumbnail} alt={char.thumbnail} />
+          <div className="char__name">{char.name}</div>
+        </li>
+      );
+    });
+
+    setData([...data, newDataList]);
+    setLoading(false);
+  }
+
+  function toUpdate() {
+    service.getAllCharacters().then(onCharListLoaded);
+  }
+
+  useEffect(toUpdate, []);
+
+  const spinnerContent = () => {
+    const spinnerArray = [];
+    for (let i = 0; i < 9; i++) {
+      spinnerArray.push(
+        <li
+          className="char__item"
+          key={id++}
+          style={{
+            padding: "25% 0",
+            backgroundColor: "white",
+          }}
+        >
+          <Spinner />
+        </li>
+      );
+    }
+
+    return spinnerArray;
+  };
+
+  const content = loading ? spinnerContent() : data;
+
+  return (
+    <div className="char__list">
+      <ul className="char__grid">{content}</ul>
+      <button className="button button__main button__long">
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
+};
 
 export default CharList;
