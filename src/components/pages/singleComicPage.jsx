670d881594e48c7efe500ce50f0af2e57a44feb1
Vladislav <gubinvladislav625@gmail.com>
2023-08-24 19:14:40 +0700

    update

--- src/components/pages/singleComicPage.jsx
+++ src/components/pages/singleComicPage.jsx
@@ -21,29 +21,11 @@ const SingleComicPage = () => {
 
   useEffect(updateData, [comicId]);
 
-  const renderComic = () => {
-    return (
-      <>
-        <img src={comic.img} alt={comic.title} className="single-comic__img" />
-        <div className="single-comic__info">
-          <h2 className="single-comic__name">{comic.title}</h2>
-          <p className="single-comic__descr">{comic.description}</p>
-          <p className="single-comic__descr">{comic.pageCount}</p>
-          <p className="single-comic__descr">{comic.language}</p>
-          <div className="single-comic__price">{comic.price}</div>
-        </div>
-        <Link to={"/comics"} className="single-comic__back">
-          Back to all
-        </Link>
-      </>
-    );
-  };
-
   const errorMessage = service.error ? <ErrorMessage /> : null;
   const spinner = service.loading ? <Spinner /> : null;
-  const content = !(service.error || service.loading || !comic)
-    ? renderComic()
-    : null;
+  const content = !(service.error || service.loading || !comic) ? (
+    <View comic={comic} />
+  ) : null;
 
   return (
     <div className="single-comic">
@@ -51,6 +33,24 @@ const SingleComicPage = () => {
       {errorMessage}
       {spinner}
     </div>
+  );
+};
+
+export const View = (comic) => {
+  return (
+    <>
+      <img src={comic.img} alt={comic.title} className="single-comic__img" />
+      <div className="single-comic__info">
+        <h2 className="single-comic__name">{comic.title}</h2>
+        <p className="single-comic__descr">{comic.description}</p>
+        <p className="single-comic__descr">{comic.pageCount}</p>
+        <p className="single-comic__descr">{comic.language}</p>
+        <div className="single-comic__price">{comic.price}</div>
+      </div>
+      <Link to={"/comics"} className="single-comic__back">
+        Back to all
+      </Link>
+    </>
   );
 };
 
